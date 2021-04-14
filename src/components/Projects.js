7cef8c64e34357862b0db6a000726870e8d599c3
Daniel Torres <daniel.torres96@hotmail.com>
2021-04-14 12:22:37 -0600

    mejoras rendimiento

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,16 +1,10 @@
 import React from 'react'
 
-import ProjectsCards from './ProjectsCards'
-import data from '../db/data.json'
+import ecommerce from '../images/projects/mouse.png'
+import dashboard from '../images/projects/admin.png'
+import landing from '../images/projects/ids.png'
 
 class Projects extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            data: data
-        }
-    }
-
     render() {
         return (
             <React.Fragment>
@@ -19,7 +13,17 @@ class Projects extends React.Component {
                     <h2>Here are a few projects I've worked. Want to see more? <a href="mailto:daniel.torres96@hotmail.com">Email me</a></h2>
 
                     <div className="Projects__cards">
-                        <ProjectsCards projects={this.state.data.projects} />
+                        <div className="ProjectsCards__container">
+                            <img src={ecommerce} alt="Project" />
+                        </div>
+
+                        <div className="ProjectsCards__container">
+                            <img src={dashboard} alt="Project" />
+                        </div>
+
+                        <div className="ProjectsCards__container">
+                            <img src={landing} alt="Project" />
+                        </div>
                     </div>
                 </div>
             </React.Fragment>
